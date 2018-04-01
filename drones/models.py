5383899ae1861c30fefbcd06b6982deaf7465388
root <csp5096@yahoo.com>
2018-03-31 20:43:17 -0500

    Added unique constraints to drones database

--- drones/models.py
+++ drones/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class DroneCategory(models.Model):
-    name = models.CharField(max_length=250)
+    name = models.CharField(max_length=250, unique=True)
 
     class Meta:
         ordering = ('name',)
@@ -10,7 +10,7 @@ class DroneCategory(models.Model):
         return self.name
 
 class Drone(models.Model):
-    name = models.CharField(max_length=250)
+    name = models.CharField(max_length=250, unique=True)
     drone_category = models.ForeignKey(
         DroneCategory,
         related_name='drones',
@@ -33,7 +33,7 @@ class Pilot(models.Model):
         (MALE, 'Male'),
         (FEMALE, 'Female'),
     )
-    name = models.CharField(max_length=150, blank=False, default='')
+    name = models.CharField(max_length=150, blank=False, unique=True)
     gender = models.CharField(
         max_length=2,
         choices=GENDER_CHOICES,
